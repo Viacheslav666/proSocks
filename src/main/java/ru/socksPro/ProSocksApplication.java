2aef3eba20be9de681f44f15ae9157fb85a38b4d
Viacheslav <119102560+Viacheslav666@users.noreply.github.com>
2023-12-11 21:40:06 +0400

    First commit

---
+++ src/main/java/ru/socksPro/ProSocksApplication.java
@@ -0,0 +1,13 @@
+package ru.socksPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/socksPro/ProSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.socksPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
